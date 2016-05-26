c1bc59681b7a9bff3a4c37da050f3179ae87fa83
evdokimoveu <evdokimoveu@gmail.com>
2016-05-26 23:01:04 +0300

    Add Intent for start Setting Activity

--- app/src/main/java/com/evdokimoveu/weather/MainActivity.java
+++ app/src/main/java/com/evdokimoveu/weather/MainActivity.java
@@ -1,11 +1,14 @@
 package com.evdokimoveu.weather;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int CODE_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +25,8 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if(id == R.menu.menu_main){
-
+            Intent intent = new Intent(this, SettingActivity.class);
+            startActivityForResult(intent, CODE_REQUEST);
             return true;
         }
         return super.onOptionsItemSelected(item);
